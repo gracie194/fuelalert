e1f2bc6059de9388a66df3444c8f778128c8226d
Uzoukwu-Michael <141550249+Uzoukwu-Michael@users.noreply.github.com>
2023-10-10 17:33:21 +0100

    Delete login.js

--- login.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('login-form').addEventListener('submit',function(event){
-  event.preventDefault()
-})
-let username = document.getElementById('username').value 
-let password = document.getElementById('password').value
