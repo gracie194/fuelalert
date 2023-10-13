154209fc69b2e34e081ba8cd614284877a1da443
Uzoukwu-Michael <141550249+Uzoukwu-Michael@users.noreply.github.com>
2023-10-13 12:18:57 +0100

    made some changes to the files

--- Admin dashboard"/dash.js
+++ Admin dashboard"/dash.js
@@ -1,37 +1,94 @@
-const navItems = document.querySelectorAll('nav li')
+// const searchInput = document.getElementById('search-input').value
+// searchInput.addEventListener('keyup',(e) =>{
+//   const seacrhData = e.target.value.tolowerCase()
+//   const filterData = categories.filter((item) => {
+//     return (
+//       item.title.tolowerCase().includes(seacrhData)
+//     )
+//   })
+//   displayItem(filterData)
+// })
+// const displayItem = (items) =>{
+
+// }
+// se 
 
 
-navItems.forEach(function(item){
-item.addEventListener('click',function(event){
-  let itemId = event.target.id 
-  navItems.forEach(function(navItem){
-    navItem.classList.remove('active')
+// // function to fetch and display statistics
+
+  let statisticsContent = document.getElementById('statisticsContent')
+
+function fetchStatistics(){
+  fetch('statistic.json')
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+    for(let i = 0; i < data.length; i++){
+    statisticsContent.innerHTML += `<h3 class="statisticsContent">${data[i].users }: ${data[i].orders }: ${data[i].revenue}</h3>`
+    }
   })
-  event.target.classList.add('active')
-  if(itemId == 'users'){
-    displayUsers()
-  }
-  else if(itemId == 'filling-stations'){
-    displayFillingStaions()
-  }
-  else if(itemId == 'error-reports'){
-    displayErrorReport()
-  }
-  else if(itemId == 'statistics'){
-    displayStatistics()
-  }
-  else if(itemId == 'blogs'){
-    displayBlog()
-  }
-})
-})
-
-function displayFillingStaions(){
-  clearContent()
-  fetch('https://')
-  .then(res => res.JSON())
-  .then(data => {
-    
-  }
-    )
+  .catch((error) => {
+    console.error('Error fetching statistics:' , error)
+  })
 }
+fetchStatistics()
+
+//function to fetch and display the admins
+
+  let adminContent = document.getElementById('adminContent')
+
+const file = 'admin.json'
+function fetchAdmin(){
+  fetch(file)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+    for(let i = 0; i < data.length; i++){
+    adminContent.innerHTML += `<h3 class="admincontent">${data[i].id }: ${data[i].username} </h3>`
+    }
+  })
+  .catch((error) => {
+    console.error('Error fetching admins:' , error)
+  })
+}
+fetchAdmin()
+
+ //function to fetch and display blogs
+
+//   let blogContent = document.getElementById('blogContent')
+// function blog(){
+//   fetch('blogs.json')
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data)
+//     for(let i = 0; i < data.length; i++){
+//     blogContent.innerHTML += `<h3 class="blogs">${data[i].title }: ${data[i].post }: ${data[i].body} </h3>`
+//     }
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching blogs:' , error)
+//   })
+// }
+// blog()
+
+// //function to fetch and display the users
+
+//   let userContent = document.getElementById('userContent')
+// function users(){
+//   fetch('users.json')
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data)
+//     for(let i = 0; i < data.length; i++){
+//     userContent.innerHTML += `<h3 class="users">${data[i].id }: ${data[i].userId }: ${data[i].firstname }: ${data[i].email }:
+//     ${data[i].password }: ${data[i].dateCreated} </h3>`
+//     }
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching blogs:' , error)
+//   })
+// }
+// users()
+
+
+
