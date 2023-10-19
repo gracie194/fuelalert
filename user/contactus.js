3724480cefd1e377e5d132fa827a76574ab39f17
gracie194 <147049840+gracie194@users.noreply.github.com>
2023-10-19 12:01:34 +0100

    Add files via upload

---
+++ user/contactus.js
@@ -0,0 +1,11 @@
+let input = document.getElementById("name");
+let input1 = document.getElementById("email");
+let input2 = document.getElementById("message");
+
+
+function sendmesssage(){
+    console.log(input.value)
+    console.log(input1.value)
+    console.log(input2.value)
+    
+}
